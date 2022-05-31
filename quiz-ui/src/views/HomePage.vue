a338a03ced7d7139f63a7a74e15aaa103a809bc4
Mathouz <mateo.pellan@gmail.com>
2022-05-31 10:41:16 +0200

    ajout premier module

---
+++ quiz-ui/src/views/HomePage.vue
@@ -0,0 +1,26 @@
+<template>
+  <h1>Home page</h1>
+
+  <router-link to="/start-new-quiz-page">Démarrer le quiz !</router-link>
+
+  <div v-for="scoreEntry in registeredScores" v-bind:key="scoreEntry.date">
+    {{ scoreEntry.playerName }} - {{ scoreEntry.score }}
+  </div>
+</template>
+
+<script>
+
+import quizApiService from "@/services/quizApiService";
+
+export default {
+  name: "HomePage",
+  data() {
+    return {
+      registeredScores : []
+    };
+  },
+  async created() {
+    console.log("Composant Home page 'created'");
+  },
+};
+</script>
