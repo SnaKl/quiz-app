8a3e2a390054f3e5bf8a5274e0ae94710c8dfd80
SnaKl <62023405+SnaKl@users.noreply.github.com>
2022-06-01 13:36:58 +0200

    Navbar component

--- quiz-ui/src/App.vue
+++ quiz-ui/src/App.vue
@@ -1,24 +1,11 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router';
+import { RouterView } from 'vue-router';
+import Navbar from '@/components/NavbarComponent.vue';
 </script>
 
 <template>
   <header>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item active">
-            <RouterLink to="/" class="nav-link">Home</RouterLink>
-          </li>
-          <li class="nav-item">
-            <RouterLink to="/about" class="nav-link">About</RouterLink>
-          </li>
-          <li class="nav-item">
-            <RouterLink to="/score" class="nav-link">Score</RouterLink>
-          </li>
-        </ul>
-      </div>
-    </nav>
+    <Navbar />
   </header>
 
   <div id="mainContainer">
---
+++ quiz-ui/src/components/NavbarComponent.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { RouterLink } from 'vue-router';
+</script>
+
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <RouterLink to="/" class="nav-link">Home</RouterLink>
+        </li>
+        <li class="nav-item">
+          <RouterLink to="/about" class="nav-link">About</RouterLink>
+        </li>
+        <li class="nav-item">
+          <RouterLink to="/score" class="nav-link">Score</RouterLink>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<style></style>
